feefc53556237d9be7a8a56b528a282954248965
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-04-19 17:20:51 +0200

    [FEAT] Modal component in the app without logic

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
 
 const App = () => {
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
