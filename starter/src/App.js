06fce503856a2c8c3ee913160f006759f8a3aa37
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-04-20 06:02:33 +0200

    [FEAT] Modal in App.js with props

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import Modal from "./components/Modal";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const { isModalOpen } = useSelector((store) => store.modal);
   return (
     <main>
-      <Modal />
+      {isModalOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
