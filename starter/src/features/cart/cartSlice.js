c158f155f16109b0bea85da7fd3edb5fe8b1cae2
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-04-20 13:34:03 +0200

    [FEAT] Fetch cart items with createAsyncThunk

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -1,8 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
+
+export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+});
 
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 0,
   total: 0,
   isLoading: true,
@@ -41,6 +48,18 @@ const cartSlice = createSlice({
       });
       state.amount = amount;
       state.total = parseFloat(total.toFixed(2));
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
     },
   },
 });
